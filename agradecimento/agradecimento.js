f6b57dddc4d136357e98a69db069f9f0cf05379d
Christian Milfont <67718947+christianmilfont@users.noreply.github.com>
2024-06-03 12:05:40 -0300

    Mais alteracoes

---
+++ agradecimento/agradecimento.js
@@ -0,0 +1,4 @@
+document.getElementById('shareBtn').addEventListener('click', function() {
+    // Implemente aqui a lógica para compartilhar nas redes sociais
+    alert('Compartilhamento nas redes sociais.');
+  });
